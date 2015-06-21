d1c600bb56051f2da9a2786505a41f056943d186
oliverhuang <oliverhdh@gmail.com>
2015-06-21 23:55:01 +0800

    support charactor

--- src/main/java/org/wltea/analyzer/core/CharacterUtil.java
+++ src/main/java/org/wltea/analyzer/core/CharacterUtil.java
@@ -54,14 +54,19 @@ class CharacterUtil {
 		}else if((input >= 'a' && input <= 'z')
 				|| (input >= 'A' && input <= 'Z')){
 			return CHAR_ENGLISH;
-			
+
 		}else {
 			Character.UnicodeBlock ub = Character.UnicodeBlock.of(input);
 			
 			if(ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS  
 					|| ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS  
-					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A){
+					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+          || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+          || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+          || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+          ){
 				//目前已知的中文字符UTF-8集合
+        // oliverhuang: 支持中文标点符号
 				return CHAR_CHINESE;
 				
 			}else if(ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS //全角数字字符和日韩字符
